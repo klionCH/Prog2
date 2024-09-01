01a52b3015a2f4e2cafa6d516cc605b1a06e8fe6
klionCH <sven.luebcke@lernende.bbw.ch>
2024-09-01 11:55:54 +0200

    tailwind setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; // Haupt-App-Komponente
+import Angeln from './pages/Angeln'; // Seite "Angeln"
+import Auto from './pages/Auto'; // Seite "Auto"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+
+function AppRouter() { // Umbenennen der Komponente
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/auto" element={<Auto />} />
+        <Route path="/angeln" element={<Angeln />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppRouter /> {/* Die umbenannte Komponente hier */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/pages/Auto.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Auto = () => {
+    return (
+        <>
+            <div className="bg-sky-500 w-full h-full">
+                awfawgaga
+            </div>
+        </>
+    );
+};
+
+export default Auto;
