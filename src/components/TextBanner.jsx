05978e4f07fad584c2304599cd60b92aefcb0a76
Sven Lübcke <sven@luebcke.ch>
2024-11-10 12:27:58 +0100

    delete

--- src/components/TextBanner.jsx
+++ src/components/TextBanner.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import '../pages/style/NeonTextBanner.css';
+import '../style/NeonTextBanner.css'; // Ensure this file has the right styles
 
-const TextBanner = ({ texts = ["Text 1", "Text 2", "Text 3"] }) => {
+const NeonBanner = ({ items , height, width}) => {
     return (
-        <div className="banner-container">
-            <div className="banner-content">
-                {texts.map((text, index) => (
-                    <span key={index} className="banner-text">{text}</span>
-                ))}
-                {/* Texte ein zweites Mal für endlose Schleife */}
-                {texts.map((text, index) => (
-                    <span key={`duplicate-${index}`} className="banner-text">{text}</span>
-                ))}
+        <div className="p-4 rounded-lg w-4/5 sm:w-3/4 md:w-1/2 lg:w-1/3 h-2" style={{height: height}}>
+            <div id="container">
+                <div className="scroll" id="scroll-one">
+                    {items.map((item, index) => (
+                        <div className="item" key={`item-one-${index}`}>
+                            {item}
+                        </div>
+                    ))}
+                </div>
+                <div className="scroll" id="scroll-two">
+                    {items.map((item, index) => (
+                        <div className="item" key={`item-two-${index}`}>
+                            {item}
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-    );
-};
+            );
+            };
 
-export default TextBanner;
+            export default NeonBanner;
